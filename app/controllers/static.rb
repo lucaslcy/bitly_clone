4b134587f32ce49b22ffa7b4b83a44337c8de362
lucaslcy <cyl.lucas@gmail.com>
2016-06-29 01:20:03 -0700

    done

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,19 +1,27 @@
 get '/' do
-  puts "[LOG] Getting /"
-  puts "[LOG] Params: #{params.inspect}"
-  @urls = Url.all
   erb :"index"
 end
 
 post '/form' do
-	@temp = Url.create(long_url: params[:long_url], short_url: Url.shorten)
-	puts @temp 
-	redirect '/'
+  @temp = Url.new(long_url: params[:long_url], short_url: Url.shorten)
+  if @temp.save
+  	erb :"index"
+  else
+    @errors = "#{@temp.errors.full_messages.join(" ")}"
+    erb :"index"
+  end
 end
 
 get '/:short_url' do
-	puts @temp 
 	@temp = Url.find_by(short_url: params[:short_url])
-	redirect "http://" + @temp.long_url
+	@temp.click_count += 1
+	@temp.save
+	
+	if @temp.long_url.match(/http[s]?/) == nil
+        redirect "http://" + "#{@temp.long_url}"
+
+    else
+        redirect "#{@temp.long_url}"
+    end
 
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,8 +1,8 @@
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 
-	validates :short_url, uniqueness: true
-	validates :long_url, uniqueness: true
+	validates :short_url, :long_url, uniqueness: {case_sensitive: false}
+	validates_format_of :long_url, :with => /(https?:\/\/.*|w{3}.*)/, :message => "Invalid_URL! Please insert valid URL starting with http:// or https:// or www."
 
 	def self.shorten
 		short_url = rand(36**rand(5..8)).to_s(36)
