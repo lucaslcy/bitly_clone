42b8522ba7a7258011dbbebfc8b81affcfb06fdf
lucaslcy <cyl.lucas@gmail.com>
2016-06-28 20:21:10 -0700

    trial

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -7,10 +7,13 @@ end
 
 post '/form' do
 	@temp = Url.create(long_url: params[:long_url], short_url: Url.shorten)
+	puts @temp 
 	redirect '/'
 end
 
 get '/:short_url' do
+	puts @temp 
 	@temp = Url.find_by(short_url: params[:short_url])
 	redirect "http://" + @temp.long_url
+
 end
