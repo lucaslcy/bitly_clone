02543338e6e3181bf453e22e2f3f73af71e75533
lucaslcy <cyl.lucas@gmail.com>
2016-07-05 00:39:43 -0700

    done

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,7 +2,7 @@ class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 
 	validates :short_url, :long_url, uniqueness: {case_sensitive: false}
-	validates_format_of :long_url, :with => /(https?:\/\/.*|w{3}.*)/, :message => "Invalid_URL! Please insert valid URL starting with http:// or https:// or www."
+	validates_format_of :long_url, :with => /(http[s]?:\/\/.*|w{3}.*)/, :message => "Invalid_URL! Please insert valid URL starting with http:// or https:// or www."
 
 	def self.shorten
 		short_url = rand(36**rand(5..8)).to_s(36)
